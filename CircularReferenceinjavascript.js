ebe6b2640a6ca806a9b51425c0be79c7aa306873
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-04-29 18:07:34 +0530

    Create CircularReferenceinjavascript.js

---
+++ CircularReferenceinjavascript.js
@@ -0,0 +1,40 @@
+console.log("Hello, World!");
+
+const obj = {
+  a: 1,
+  b: 2
+};
+
+obj.c = obj;
+
+const removeCyclicRef = (obj) => {
+  const visited = new WeakSet();
+
+  const traverseData = (data) => {
+    let result;
+
+    if (visited.has(data)) {
+      return;
+    }
+
+    if (typeof data === 'object') {
+      visited.add(data);
+      result = Array.isArray(data) ? [] : {};
+      for (let key in data) {
+        const stagedResult = traverseData(data[key]);
+        if (stagedResult) {
+          result[key] = stagedResult;
+        }
+      }
+    } else {
+      result = data;
+    }
+
+    return result;
+  };
+
+  return traverseData(obj); // Return the result of the traversal
+};
+
+const newObj = removeCyclicRef(obj); // Save the result in a new object
+console.log(newObj);
