25e002325b63b1152a8f03046a456d049f6c105f
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-06-13 16:53:57 +0530

    Update CircularReferenceinjavascript.js

--- CircularReferenceinjavascript.js
+++ CircularReferenceinjavascript.js
@@ -7,27 +7,38 @@ obj.property = obj;
 obj.property2 = obj;
 
 const RemoveCyclicRef = (obj) => {
+  //weakset which keeps track of objects
   const visited = new WeakSet();
   
   const traverseData = (data) => {
+    // create a new object that needs to be returned
     let result = {}
-    
+    //if object is already there in WeakSet then return 
     if(visited.has(data)){
       return;
     }
     
+    //if the type of key is object then
     if(typeof data === 'object'){
+      //add to the set
       visited.add(data);
       
+      //loop the object
       for(key in data){
+        
+        //get the return value of the traversedData
         const stagedResult = traverseData(data[key]);
         
+        //if it is not undefined or null then 
         if(stagedResult){
           
+        //create a new key and and add the value  
         result[key] = stagedResult;
         }
       }
     } else {
+      
+      //directly add the primitive value
       result = data;
     }
   
