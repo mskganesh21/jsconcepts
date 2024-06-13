f4fcba7b7a27ad8b33c2812a986612158bf77832
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-06-13 16:49:04 +0530

    Update CircularReferenceinjavascript.js

--- CircularReferenceinjavascript.js
+++ CircularReferenceinjavascript.js
@@ -1,39 +1,44 @@
-console.log("Hello, World!");
-
 const obj = {
-  a: 1,
-  b: 2
-};
+  name: 'sai',
+  age: 27
+}
 
-obj.c = obj;
+obj.property = obj;
+obj.property2 = obj;
 
-const removeCyclicRef = (obj) => {
+const RemoveCyclicRef = (obj) => {
   const visited = new WeakSet();
-
+  
   const traverseData = (data) => {
     let result = {}
-
-    if (visited.has(data)) {
+    
+    if(visited.has(data)){
       return;
     }
-
-    if (typeof data === 'object') {
+    
+    if(typeof data === 'object'){
       visited.add(data);
-      for (let key in data) {
+      
+      for(key in data){
         const stagedResult = traverseData(data[key]);
-        if (stagedResult) {
-          result[key] = stagedResult;
+        
+        if(stagedResult){
+          
+        result[key] = stagedResult;
         }
       }
     } else {
       result = data;
     }
+  
+      return result;
+  }
+  
+return traverseData(obj);
+  
+  
+}
 
-    return result;
-  };
 
-  return traverseData(obj); // Return the result of the traversal
-};
-
-const newObj = removeCyclicRef(obj); // Save the result in a new object
-console.log(newObj);
+const result = RemoveCyclicRef(obj);
+console.log(result);
