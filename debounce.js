40f4939edbe937e354b3ca844b1e394e43752dfd
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-06-14 13:05:09 +0530

    Create debounce.js

---
+++ debounce.js
@@ -0,0 +1,10 @@
+const debounce = (cb, delay) => {
+  let timeout;
+  
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+};
